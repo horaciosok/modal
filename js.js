c2fdcdd3a8024a442155c01bbd800e68103a028b
horaciosok <44445065+horaciosok@users.noreply.github.com>
2021-01-06 10:12:17 -0300

    Add files via upload

---
+++ js.js
@@ -0,0 +1,6 @@
+document.querySelector(".bt").addEventListener("click", () => {
+    document.querySelector(".modal-cont").classList.toggle('modal-active')
+})
+document.querySelector(".modals button").addEventListener("click", () => {
+    document.querySelector(".modal-cont").classList.toggle('modal-active')
+})
